5570d120a07bcf30a5a98de230f185754246a557
David Calaiò <davidcjr11@gmail.com>
2023-03-20 16:45:18 +0100

    fixed price-discount-finalprice and add name-last name

--- js/script.js
+++ js/script.js
@@ -1,27 +1,58 @@
-
+// prezzo biglietto per km
 const kmPrice = 0.21;
 console.log('kmPrice', kmPrice);
-
+// sconto <18
 const disc20 = 20;
-console.log('disc20', disc20);
-
+//sconto >65
 const disc40 = 40;
-console.log('disc40', disc40);
-
-let km = 'kmUtente'
-console.log('km', km);
-
-
+//nome utente
+const name = prompt('Inserisci il tuo nome')
+//cognome utente
+const lastname = prompt('Inserici il tuo cognome')
 
 
 
 
-
-
-
-
-const age = prompt('Quanti anni hai?')
+let age = parseInt(prompt('Quanti anni hai?'));
 console.log('age', age);
 
-const kmUtente = prompt('Quanti km devi percorrere?')
-console.log('kmUtente', kmUtente)
+//calcolo prezzo km x km utente 
+let kmUtente = parseInt(prompt('Quanti km devi percorrere?', 100));
+console.warn('kmUtente', kmUtente)
+
+
+let price =  kmPrice * kmUtente;
+console.log('price', price);
+
+let newPrice = price;
+
+if(age < 18) {
+    newPrice = price - (price / 100 * disc20);
+    console.log('sconto under');
+}else if (age > 65) {
+    newPrice = price - (price / 100 * disc40);
+    console.log('sconto over');
+}
+console.log('prezzo finale', newPrice);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
