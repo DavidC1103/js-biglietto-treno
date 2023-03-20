585f8c93630bcab6c5d0a868d7acf7abe5404f07
David Calaiò <davidcjr11@gmail.com>
2023-03-20 15:11:35 +0100

    add const and let in js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+const kmPrice = 0.21;
+console.log('kmPrice', kmPrice);
+
+const disc20 = 20;
+console.log('disc20', disc20);
+
+const disc40 = 40;
+console.log('disc40', disc40);
+
+let km = 'kmUtente'
+console.log('km', km);
+
+
+
+
+
+
+
+
+
+
+const age = prompt('Quanti anni hai?')
+console.log('age', age);
+
+const kmUtente = prompt('Quanti km devi percorrere?')
+console.log('kmUtente', kmUtente)
